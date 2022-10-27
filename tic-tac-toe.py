20f8dd483fc531ffb37b6638cdd5c2bb61a156e5
latif98 <104978949+latif98@users.noreply.github.com>
2022-10-27 12:24:41 +0800

    Add files via upload

---
+++ tic-tac-toe.py
@@ -0,0 +1,147 @@
+#  A simple Tic-Tac-Toe game
+# Players 'X' and 'O' take turn inputing their position on the command line using numbers 1-9
+# 1 | 2 | 3
+# ---------
+# 4 | 5 | 6
+# ---------
+# 7 | 8 | 9
+#
+
+# The Game Board 
+board = {
+    1: ' ', 2: ' ', 3: ' ',
+    4: ' ', 5: ' ', 6: ' ',
+    7: ' ', 8: ' ', 9: ' '
+}
+
+# TODO: update the gameboard with the user input
+def markBoard(position, mark):
+    position = int(position)
+    if board[position] == ' ':
+        board[position] = mark
+
+    
+# TODO: print the game board as described at the top of this code skeleton
+# Will not be tested in Part 1
+def printBoard(board):
+    copyBoard = {
+    1: ' ', 2: ' ', 3: ' ',
+    4: ' ', 5: ' ', 6: ' ',
+    7: ' ', 8: ' ', 9: ' '
+}
+    for i in copyBoard.keys():
+        if board[i] == ' ':
+            # change space to num
+            copyBoard[i] = i
+        else:
+            copyBoard[i] = board[i]
+    print( '\n' +
+        str(copyBoard[1]) + " | " + str(copyBoard[2]) + " | " + str(copyBoard[3]) + '\n' +
+        ' --------- \n' +
+        str(copyBoard[4]) + " | " + str(copyBoard[5]) + " | " + str(copyBoard[6]) + '\n' +
+        ' --------- \n' +
+        str(copyBoard[7]) + " | " + str(copyBoard[8]) + " | " + str(copyBoard[9]) + '\n'
+    )
+    return 
+
+
+# TODO: check for wrong input, this function should return True or False.
+# True denoting that the user input is correct
+# you will need to check for wrong input (user is entering invalid position) or position is out of bound
+# another case is that the position is already occupied
+def validateMove(position):
+    position = int(position)
+    if position >= 1 and position <= 9:
+        if board[position] == ' ':
+            return True
+
+    return False
+
+
+# TODO: list out all the combinations of winning, you will neeed this
+# one of the winning combinations is already done for you
+winCombinations = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+]
+
+# TODO: implement a logic to check if the previous winner just win
+# This method should return with True or False
+def checkWin(player):
+    for win in winCombinations:
+        if board[win[0]] == player and board[win[1]] == player and board[win[2]] == player:
+            return True
+    
+    return False
+
+
+# TODO: implement a function to check if the game board is already full
+# For tic-tac-toe, tie bascially means the whole board is already occupied
+# This function should return with boolean
+def checkFull(board):
+    for cell in board:
+        if board[cell] == ' ':
+            return False
+
+    return True
+
+
+#########################################################
+## Copy all your code/fucntions in Part 1 to above lines
+## (Without Test Cases)
+#########################################################
+def main():
+    gameEnded = False
+    currentTurnPlayer = 'X'
+    # entry point of the whole program
+    print('Game started: \n\n' +
+        ' 1 | 2 | 3 \n' +
+        ' --------- \n' +
+        ' 4 | 5 | 6 \n' +
+        ' --------- \n' +
+        ' 7 | 8 | 9 \n')
+    # TODO: Complete the game play logic below
+    # You could reference the following flow
+    # 1. Ask for user input and validate the input
+    # 2. Update the board
+    # 3. Check win or tie situation
+    # 4. Switch User
+    while not gameEnded:
+        move = input(currentTurnPlayer + "'s turn, input: ")
+        if not move.isnumeric():
+            print("Please Enter a Number 1-9!")
+            continue
+        if not validateMove(move):
+            print("Invalid, please try again")
+            continue
+        markBoard(move,currentTurnPlayer)
+        printBoard(board)
+        if checkWin(currentTurnPlayer):
+            print("Player " + currentTurnPlayer + " win the game")
+            break
+        if checkFull(board):
+            print("Its a tie")
+            break
+        if currentTurnPlayer == 'X':
+            currentTurnPlayer = 'O'
+        else:
+            currentTurnPlayer = 'X'
+    # Bonus Point: Implement the feature for the user to restart the game after a tie or game over        
+    restart = input("Do you want to restart the game(y/n)?" ).lower()
+    for i in board:
+        if i <= 9 :
+            board[i] = ' '
+            continue
+    if restart == 'y':
+        main()
+    else:
+        exit()
+
+main()
+    
